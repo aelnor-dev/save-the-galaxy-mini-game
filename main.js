c14b1181998d608fffa0f98a26c86df02d28ad76
Aelnordev <loreriba99@gmail.com>
2025-02-13 20:47:06 +0100

    new diagonal jump + fix collisions

--- main.js
+++ main.js
@@ -10,19 +10,42 @@ class Game {
     this.addEvent();
     this.alienGenerator();
     this.over = false;
+    this.keysPressed = new Set();
   }
   createScene() {
     this.character = new Character();
     this.container.appendChild(this.character.element);
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 10; i++) {
       const coin = new Coin();
       this.coins.push(coin);
       this.container.appendChild(coin.element);
     }
   }
   addEvent() {
-    window.addEventListener("keydown", (e) => this.character.moves(e));
+    window.addEventListener("keydown", (e) => this.processKeyDown(e));
+    window.addEventListener("keyup", (e) => this.processKeyUp(e));
+    this.checkMovements();
     this.checkCollisions();
+  }
+
+  processKeyDown(event) {
+    console.log("keydown");
+    const allowedKeys = ["ArrowRight", "ArrowLeft", "ArrowUp"];
+    if (allowedKeys.includes(event.key)) {
+      this.keysPressed.add(event.key);
+    }
+  }
+
+  processKeyUp(event) {
+    if (this.keysPressed.has(event.key)) {
+      this.keysPressed.delete(event.key);
+    }
+  }
+
+  checkMovements() {
+    setInterval(() => {
+      this.character.moves(this.keysPressed);
+    }, 100);
   }
 
   checkCollisions() {
@@ -41,7 +64,7 @@ class Game {
 
       this.aliens.forEach((alien) => {
         if (this.character.collidesWith(alien)) {
-          this.gameOver(); 
+          this.gameOver();
         }
       });
     }, 100);
@@ -49,46 +72,45 @@ class Game {
 
   updateScore(score) {
     this.score += score;
-    this.scoreElement.textContent = `Estrellas recolectadas: ${this.score}/5`;
-    if (this.score === 5) {
+    this.scoreElement.textContent = `Estrellas recolectadas: ${this.score}/10`;
+    if (this.score === 10) {
       this.win();
     }
   }
 
   alienGenerator() {
-
     setInterval(() => {
-      if (this.aliens.length < 2) { 
+      if (this.aliens.length < 2) {
         this.spawnAlien();
-        console.log("hola desde el while")
+        console.log("hola desde el while");
       }
-    }, 1000); 
+    }, 1000);
   }
 
   spawnAlien() {
     const side = Math.random() < 0.5 ? "left" : "right";
 
-    let alien 
+    let alien;
     const type = Math.random();
     if (type < 0.5) {
-        alien = new CrawlingAlien(side); 
+      alien = new CrawlingAlien(side);
     } else {
-        alien = new FlyingAlien(side); 
+      alien = new FlyingAlien(side);
     }
 
     this.aliens.push(alien);
     this.container.appendChild(alien.element);
 
     const moveInterval = setInterval(() => {
-        if (!alien.offScreen()) {
-            alien.moves();
-        } else {
-            clearInterval(moveInterval);
-            if (this.container.contains(alien.element)) {
-                this.container.removeChild(alien.element);
-            }
-            this.aliens = this.aliens.filter((a) => a !== alien);
+      if (!alien.offScreen()) {
+        alien.moves();
+      } else {
+        clearInterval(moveInterval);
+        if (this.container.contains(alien.element)) {
+          this.container.removeChild(alien.element);
         }
+        this.aliens = this.aliens.filter((a) => a !== alien);
+      }
     }, 20);
   }
 
@@ -121,8 +143,8 @@ class Character {
   constructor() {
     this.x = 250;
     this.y = 215;
-    this.width = 90;
-    this.height = 90;
+    this.width = 60; //no coinciden tamaños con el CSS porque sino chocaba con el espacio de la imagen
+    this.height = 60;
     this.speed = 10;
     this.jumping = false;
     this.element = document.createElement("div");
@@ -135,25 +157,27 @@ class Character {
     this.falling = false;
   }
 
-  moves(event) {
+  moves(keysPressed) {
     const container = document.getElementById("game-container");
     const containerWidth = container.offsetWidth;
-    const containerHeight = container.offsetHeight;
 
-    if (event.key === "ArrowRight" ) {
-      if (this.x + this.width + this.speed <= containerWidth) {
-        // Right boundary
-        this.x += this.speed;
-        this.facingRight = true;
+    console.log("keys pressed", keysPressed);
+    for (const key of keysPressed.values()) {
+      if (key === "ArrowRight") {
+        if (this.x + this.width + this.speed <= containerWidth) {
+          // Límite dcha
+          this.x += this.speed;
+          this.facingRight = true;
+        }
+      } else if (key === "ArrowLeft") {
+        if (this.x - this.speed >= 0) {
+          // Límite izq
+          this.x -= this.speed;
+          this.facingRight = false;
+        }
+      } else if (key === "ArrowUp") {
+        this.jumps();
       }
-    } else if (event.key === "ArrowLeft") {
-      if (this.x - this.speed >= 0) {
-        // Left boundary
-        this.x -= this.speed;
-        this.facingRight = false;
-      }
-    } else if (event.key === "ArrowUp") {
-      this.jumps();
     }
 
     this.updatePosition();
@@ -163,7 +187,7 @@ class Character {
     if (!this.jumping && (this.canJumpInTheAir || !this.falls)) {
       if (this.falling) {
         this.canJumpInTheAir = false; // Ya usó el salto en el aire
-        clearInterval(this.gravityInterval); // Interrumpe la caida
+        clearInterval(this.gravityInterval); // Interrumpe la caída
         this.gravityInterval = null;
         this.falling = false;
       }
@@ -195,7 +219,7 @@ class Character {
         clearInterval(this.gravityInterval);
         this.gravityInterval = null;
         this.falling = false;
-        this.canJumpInTheAir = true; 
+        this.canJumpInTheAir = true;
         this.y = 215;
         this.updatePosition();
         return;
@@ -240,9 +264,9 @@ class Coin {
 
 class Alien {
   constructor(side) {
-    this.width = 50;
-    this.height = 60;
-    this.speed = Math.random() * 1.5 + 0.5
+    this.width = 10;
+    this.height = 10;
+    this.speed = Math.random() * 2 + 1;
     this.side = side; // "left" o "right"
     this.element = document.createElement("div");
     this.element.classList.add("alien");
@@ -273,12 +297,11 @@ class Alien {
   }
 }
 
-
 class CrawlingAlien extends Alien {
   constructor(side) {
-    super(side); //solo necesita side como parámetro, las demas propiedades ya están cogidas del padre
-    this.element.classList.add("crawling-alien"); 
-    this.y = 250; 
+    super(side); //Solo necesita side como parámetro, las demas propiedades ya están cogidas del padre
+    this.element.classList.add("crawling-alien");
+    this.y = 250;
     this.updatePosition();
   }
 }
@@ -286,24 +309,23 @@ class CrawlingAlien extends Alien {
 class FlyingAlien extends Alien {
   constructor(side) {
     super(side);
-    this.element.classList.add("flying-alien"); 
-    this.y = 50;  
+    this.element.classList.add("flying-alien");
+    this.y = 90;
     this.updatePosition();
   }
 }
 
-
 window.showGame = showGame;
 function showGame() {
-  const showGame = document.getElementById("showGame");
+  document.getElementById("showGame");
   document.getElementById("description").style.display = "none";
   document.getElementById("game").style.display = "flex";
-  const gaming = new Game();
+  new Game();
 }
 
 const bgMusic = new Audio("background-music.mp3");
 bgMusic.loop = true;
-bgMusic.volume = 0.1; // Adjust volume (0.0 - 1.0)
+bgMusic.volume = 0.1;
 bgMusic.play();
 const coinSound = new Audio("coin-sound.mp3");
 coinSound.volume = 0.5;
@@ -311,4 +333,3 @@ const jumpSound = new Audio("jump-sound.mp3");
 jumpSound.volume = 0.5;
 const loseSound = new Audio("lose-sound.mp3");
 loseSound.volume = 0.5;
-
